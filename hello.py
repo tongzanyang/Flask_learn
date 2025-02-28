ba77c3545016c027591a027e2dc6688a2a7967a0
故河 <127592981+tongzanyang@users.noreply.github.com>
2025-02-28 16:12:49 +0800

    update

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 # @Version  : 1.0
 # @Author   : 故河
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
 
 class config(object):
     SECREY_KEY = "t04241003+"
@@ -28,7 +28,11 @@ key = app.config["SECREY_KEY"]
 
 @app.route("/")
 def test():
-    return f"<h1>Hello World{key}</h1>"
+    response = make_response(f"<h1>Hello World{key}</h1>")
+    response.headers['X-Custom-Header'] = 'Value'
+    response.status = 200
+    return response
+
 
 if __name__ == '__main__':
     app.run()
