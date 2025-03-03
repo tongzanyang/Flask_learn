eec2d9b62f22b38e4a689efc1438bc68e1a96045
故河 <127592981+tongzanyang@users.noreply.github.com>
2025-03-03 11:52:42 +0800

    update

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 # @Version  : 1.0
 # @Author   : 故河
 
-from flask import Flask, request, render_template, make_response
+from flask import Flask, request, make_response, session
 
 class config(object):
-    SECREY_KEY = "t04241003+"
+    SECRET_KEY = "t04241003+"
 
 class config1(config):
     debug = True
@@ -21,19 +21,53 @@ app.config.from_object(config1)
 # 3.环境变量
 
 
-key = app.config["SECREY_KEY"]
-
-
+key = app.config["SECRET_KEY"]
 
 
 @app.route("/")
 def test():
+    # 自定义响应信息
     response = make_response(f"<h1>Hello World{key}</h1>")
     response.headers['X-Custom-Header'] = 'Value'
     response.status = 200
     return response
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/get_data', methods=['POST','GET'])
+def get_data():
+    data = request.args.get('name')   # 记录请求的参数
+    return f"{data}"
 
+
+@app.route('/cookie')
+def set_cookie():
+    # 如何设置cookie,通过创建响应对象，在响应之前对响应内容进行cookie设置
+    response = make_response("set cookie success")
+    response.set_cookie('name', 'tzy', max_age=100)
+    # response.delete_cookie()
+    return response
+
+@app.route("/get_cookie")
+def get_cookie():
+    cookies = request.cookies.get('name')
+    return f"hello {cookies}"
+
+
+# 设置session，前提设置一个secret key
+@app.route('/set_session')
+def set_session():
+    session['name'] = 'tzy'
+    return "set session success"
+
+# 获取session
+@app.route('/get_session')
+def get_session():
+    name = session.get('name')
+    return f'get session name {name}'
+
+
+
+
+if __name__ == '__main__':
+    # debug模式运行
+    app.run(debug=True)
